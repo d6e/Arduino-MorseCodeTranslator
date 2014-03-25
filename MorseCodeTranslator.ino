949b369c111d0fbfef8b792f175c7c834bf0d19e
Danielle Jenkins <jenkinda@uw.edu>
2014-03-25 15:52:05 -0700

    Added beginnings of morse code translate function, did some refactoring, and added some functions to handle gaps

--- MorseCodeTranslator.ino
+++ MorseCodeTranslator.ino
@@ -1,4 +1,4 @@
-/* 
+  /* 
  Morse code translator.
  
  For the Arduino Leonardo and Micro.
@@ -17,31 +17,57 @@ char morseChar[maxMorseCharSize];  // the string of dits and dahs used to repres
 int morseCharSize = 0;
 const int timeUnit = 100;           // time unit for beats in milliseconds
 
-void appendDit()
+void appendMorseChar(char c)
 { 
   //TODO: Check if morseCharSize is too large. Maybe print an error
-  morseChar[morseCharSize] = '.';  // add a dit to the end of the string
-  ++morseCharSize;                 // increment the string size
-}
-
-void appendDah()
-{ 
-  //TODO: Check if morseCharSize is too large. Maybe print an error
-  morseChar[morseCharSize] = '-';  // add a dah to the end of the string
+  morseChar[morseCharSize] = c;  // add a dit to the end of the string
   ++morseCharSize;                 // increment the string size
 }
 
 void appendDitOrDah(int held) 
 // determines whether dit, dah, or space and appends
 {
-  if (held < timeUnit*2)           // if the button was released in less than a second 
+  if (held <= 2)           
   {
-    appendDit();                   // append a dit to the morseChar string
+    appendMorseChar('.');                   // append a dit to the morseChar string
   }
   else                             // a second has passed, note that the button may still be down
   {
-    appendDah();                   // append a dah to the morseChar string
+    appendMorseChar('-');                   // append a dah to the morseChar string
   } 
+}
+
+void handleGap(int unheld)
+// Handles gap behavior appropriately
+{
+    if (unheld >= 7) // if word gap
+    {
+       Keyboard.write(' ');  // send a keyboard space char 
+    }
+    else if (unheld >= 3 && unheld < 7) // if letter gap
+    {
+        Keyboard.write(translateMorse());  // translate morse code to keyboard char
+    }
+}
+
+char translateMorse()
+// translates dits and dahs to ascii chars
+{
+    if (morseChar[0] == '.')
+    {
+        if (morseChar[1] == '.')
+        {
+
+        }
+        else if (morseChar[1] == '-')
+        {
+
+        }
+        else
+        {
+            return 'e';
+        }
+    }
 }
 
 void setup() 
@@ -63,11 +89,12 @@ void loop()
   appendDitOrDah(held);                   // determines whether dit, dah, or space and appends
 
   // count the number of unheld beats
-  while (digitalRead(button) != HIGH && unheld < )
+  while (digitalRead(button) != HIGH && unheld < 30)
   {
     delay(timeUnit);                       // count out one time unit
     ++unheld;
   } 
+  handleGap(unheld);
 }
 
 
